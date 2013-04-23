133d3876c95118bc9eaf3a69143b40fe5c70bdc0
Charlie Knudsen <charlie.knudsen@gmail.com>
2013-04-23 01:54:56 -0500

    Push output to redis, setup basic log viewer

--- run.go
+++ run.go
@@ -2,23 +2,50 @@ package main
 
 import (
 	"bufio"
+	"bytes"
 	"crypto/rand"
 	"flag"
 	"fmt"
 	"github.com/drone/routes"
+	"github.com/garyburd/redigo/redis"
+	"html/template"
 	"io"
 	"log"
 	"net/http"
 	"os"
 	"os/exec"
+	"time"
 )
 
 var addr = flag.String("addr", ":8088", "http service address")
+
+var homeTempl = template.Must(template.ParseFiles("templates/task.html"))
+
+var redisServer = "localhost:6379"
+var pool = &redis.Pool{
+	MaxIdle:     3,
+	IdleTimeout: 240 * time.Second,
+	Dial: func() (redis.Conn, error) {
+		c, err := redis.Dial("tcp", redisServer)
+		if err != nil {
+			return nil, err
+		}
+		return c, err
+	},
+	TestOnBorrow: func(c redis.Conn, t time.Time) error {
+		_, err := c.Do("PING")
+		return err
+	},
+}
 
 type TaskExecution struct {
 	Service string `json:"service"`
 	Task    string `json:"task"`
 	TaskId  string `json:"task_id"`
+}
+
+func (task *TaskExecution) RedisKey() string {
+	return fmt.Sprintf("task:%s", task.TaskId)
 }
 
 type logLine struct {
@@ -32,11 +59,37 @@ func generateId() string {
 	return fmt.Sprintf("%x", buf)
 }
 
-func taskRoute(w http.ResponseWriter, r *http.Request) {
+func executeRoute(w http.ResponseWriter, r *http.Request) {
 	params := r.URL.Query()
 	task := &TaskExecution{params.Get(":service"), params.Get(":task"), generateId()}
-	executeTask(task)
-	routes.ServeJson(w, task)
+	go executeTask(task)
+	http.Redirect(w, r, fmt.Sprintf("/task/%s", task.TaskId), 302)
+}
+
+func taskRoute(w http.ResponseWriter, r *http.Request) {
+	params := r.URL.Query()
+	conn := pool.Get()
+	defer conn.Close()
+	var buffer bytes.Buffer
+
+	// TODO get list and read all lines
+	// TODO allow client to specify offset
+	// TODO send client next key to look for
+	task := &TaskExecution{TaskId: params.Get(":taskId")}
+	lines, err := redis.Strings(conn.Do("LRANGE", task.RedisKey(), 0, 4000))
+	var cmdOut string
+	if err != nil {
+		cmdOut = err.Error()
+	} else {
+		for _, line := range lines {
+			buffer.WriteString(line)
+		}
+		cmdOut = buffer.String()
+	}
+	ctx := map[string]string{
+		"cmdOut": cmdOut,
+	}
+	homeTempl.Execute(w, ctx)
 }
 
 func startWebserver() {
@@ -46,18 +99,20 @@ func startWebserver() {
 		log.Fatal(err)
 	}
 	mux.Static("/static/", cwd)
-	mux.Get("/api/:service/:task", taskRoute)
+	mux.Get("/execute/:service/:task", executeRoute)
+	mux.Get("/task/:taskId", taskRoute)
 
 	http.Handle("/", mux)
+	log.Printf("http server started on port %s", *addr)
 	http.ListenAndServe(*addr, nil)
 }
 
 func executeTask(task *TaskExecution) {
-	cmd := exec.Command("echo", "-n", "run.go\ntest\ntest\n")
+	cmd := exec.Command("/bin/bash", "test.sh")
 	commandOut := make(chan string)
-	go printOutput(commandOut)
+	defer close(commandOut)
+	go printOutput(task, commandOut)
 	captureOutput(cmd, commandOut)
-	close(commandOut)
 }
 
 func captureOutput(cmd *exec.Cmd, commandOut chan string) error {
@@ -79,12 +134,17 @@ func captureOutput(cmd *exec.Cmd, commandOut chan string) error {
 			break
 		}
 	}
+	// TODO write status code to output
 	return cmd.Wait()
 }
 
-func printOutput(commandOut chan string) {
+func printOutput(task *TaskExecution, commandOut chan string) {
+	conn := pool.Get()
+	defer conn.Close()
+	fmt.Println(task.RedisKey())
 	for line := range commandOut {
-		fmt.Printf("%s", line)
+		fmt.Println(line)
+		conn.Do("RPUSH", task.RedisKey(), line)
 	}
 }
 
